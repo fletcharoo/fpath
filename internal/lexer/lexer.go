26b69bc32034a260e49d178b652e1334147b1946
FletcherCutting <fcutting@hotmail.com>
2024-11-19 21:04:46 +1100

    Add lexer

---
+++ internal/lexer/lexer.go
@@ -0,0 +1,233 @@
+package lexer
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"unicode"
+)
+
+const (
+	TokenType_Undefined = iota
+	TokenType_Number
+	TokenType_Label
+	TokenType_StringLiteral
+	TokenType_Plus
+)
+
+var (
+	TokenTypeString map[int]string = map[int]string{
+		TokenType_Undefined:     "Undefined",
+		TokenType_Number:        "Number",
+		TokenType_Label:         "Label",
+		TokenType_StringLiteral: "StringLiteral",
+		TokenType_Plus:          "Plus",
+	}
+
+	errUnexpectedEOF = errors.New("unexpected EOF")
+	errInvalidRune   = errors.New("invalid rune")
+)
+
+// isLabelRune returns whether the provided rune is a valid label rune.
+// Valid label runes are letters, numbers, and underscores.
+func isLabelRune(r rune) bool {
+	return unicode.IsLetter(r) || unicode.IsNumber(r) || r == '_'
+}
+
+type Token struct {
+	Type  int
+	Value string
+}
+
+// NewLexer returns a new Lexer configured to read from a slice
+// []rune value of the input string.
+func NewLexer(input string) *Lexer {
+	return &Lexer{
+		input: []rune(input),
+	}
+}
+
+// Lexer adds the functionality to get and peek tokens from a
+// string using a buffer.
+type Lexer struct {
+	input []rune
+	index int
+	buf   *Token
+}
+
+// getRune returns the rune at the current index of the input and increments the
+// index.
+// If the index is larger than the length of the input, getRune returns an
+// io.EOF error.
+func (l *Lexer) getRune() (r rune, err error) {
+	if l.index == len(l.input) {
+		return 0, io.EOF
+	}
+
+	r = l.input[l.index]
+	l.index++
+	return r, nil
+}
+
+// peekRune returns the rune at the current index of the input but doesn't
+// increment the index.
+// If the index is larger than the length of the input, peekRune returns an
+// io.EOF error.
+func (l *Lexer) peekRune() (r rune, err error) {
+	if l.index == len(l.input) {
+		return 0, io.EOF
+	}
+
+	return l.input[l.index], nil
+}
+
+// getToken returns the next token in the input string.
+// If there are no more tokens to process in the string, getToken returns an
+// io.EOF error.
+func (l *Lexer) GetToken() (tok Token, err error) {
+	if l.buf != nil {
+		tok = *l.buf
+		l.buf = nil
+		return tok, nil
+	}
+
+	var r rune
+
+	for {
+		r, err = l.peekRune()
+
+		if err != nil {
+			return tok, err
+		}
+
+		if unicode.IsSpace(r) {
+			l.index++
+			continue
+		}
+
+		if unicode.IsNumber(r) {
+			return l.getTokenNumber()
+		}
+
+		if isLabelRune(r) {
+			return l.getTokenLabel()
+		}
+
+		switch r {
+		case '"':
+			l.index++
+			return l.getTokenStringLiteral()
+		case '+':
+			l.index++
+			return Token{
+				Type: TokenType_Plus,
+			}, nil
+		default:
+			err = fmt.Errorf("%w: %s", errInvalidRune, string(r))
+			return
+		}
+	}
+}
+
+// peekToken returns the current token in the input string but does not
+// increment the index.
+// If there are no more tokens to process in the string, getToken returns an
+// io.EOF error.
+func (l *Lexer) PeekToken() (tok Token, err error) {
+	if l.buf != nil {
+		tok = *l.buf
+		return tok, nil
+	}
+
+	tok, err = l.GetToken()
+	l.buf = &tok
+	return tok, err
+}
+
+// getTokenNumber returns the current number token in the input string.
+// If there are no more tokens to process in the string, getToken returns an
+// io.EOF error.
+func (l *Lexer) getTokenNumber() (tok Token, err error) {
+	tok.Type = TokenType_Number
+	var r rune
+
+	for {
+		r, err = l.peekRune()
+
+		if err == io.EOF {
+			return tok, nil
+		}
+
+		if err != nil {
+			return tok, err
+		}
+
+		if unicode.IsNumber(r) {
+			l.index++
+			tok.Value += string(r)
+			continue
+		}
+
+		return tok, nil
+	}
+}
+
+// getTokenLabel returns the current label token in the input string.
+// If there are no more tokens to process in the string, getToken returns an
+// io.EOF error.
+func (l *Lexer) getTokenLabel() (tok Token, err error) {
+	tok.Type = TokenType_Label
+	var r rune
+
+	for {
+		r, err = l.peekRune()
+
+		if err != nil {
+			break
+		}
+
+		if isLabelRune(r) {
+			l.index++
+			tok.Value += string(r)
+			continue
+		}
+
+		break
+	}
+
+	if err == io.EOF {
+		return tok, nil
+	}
+
+	return tok, err
+}
+
+// getTokenStringLiteral returns the current string literal token in the input
+// string.
+// If the token reaches the end of the string, getTokenStringLiteral returns an
+// UnexpectedEOF error.
+func (l *Lexer) getTokenStringLiteral() (tok Token, err error) {
+	tok.Type = TokenType_StringLiteral
+	var r rune
+
+	for {
+		r, err = l.getRune()
+
+		if err == io.EOF {
+			err = errUnexpectedEOF
+			return
+		}
+
+		if err != nil {
+			return
+		}
+
+		if r == '"' {
+			break
+		}
+
+		tok.Value += string(r)
+	}
+
+	return tok, nil
+}
---
+++ internal/lexer/lexer_test.go
@@ -0,0 +1,348 @@
+package lexer
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"testing"
+)
+
+func _tokensMatch(expected, actual Token) (err error) {
+	if expected.Type != actual.Type {
+		err = fmt.Errorf("Unexpected type\nExpected: %s\nActual: %s", TokenTypeString[expected.Type], TokenTypeString[actual.Type])
+		return
+	}
+
+	if expected.Value != actual.Value {
+		err = fmt.Errorf("Unexpected value\nExpected: %s\nActual: %s", expected.Value, actual.Value)
+	}
+
+	return nil
+}
+
+func Test_isLabelRune(t *testing.T) {
+	testCases := map[string]struct {
+		r        rune
+		expected bool
+	}{
+		"1": {
+			r:        '1',
+			expected: true,
+		},
+		"f": {
+			r:        'f',
+			expected: true,
+		},
+		"_": {
+			r:        '_',
+			expected: true,
+		},
+		"$": {
+			r:        '$',
+			expected: false,
+		},
+		"-": {
+			r:        '-',
+			expected: false,
+		},
+		"Tab": {
+			r:        '\t',
+			expected: false,
+		},
+		"Space": {
+			r:        ' ',
+			expected: false,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			result := isLabelRune(tc.r)
+			if result != tc.expected {
+				t.Fatalf("Unexpected result\nExpected: %v\nActual: %v", tc.expected, result)
+			}
+		})
+	}
+}
+
+func Test_Lexer_getRune(t *testing.T) {
+	input := "hello world"
+	expectedRunes := []rune{'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'}
+	lexer := NewLexer(input)
+
+	for i, expected := range expectedRunes {
+		r, err := lexer.getRune()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if r != expected {
+			t.Fatalf("Unexpected rune at position %d\nExpected: %q\n Actual: %q", i, expected, r)
+		}
+	}
+}
+
+func Test_Lexer_getRune_EOF(t *testing.T) {
+	input := "h"
+	expected := 'h'
+	lexer := NewLexer(input)
+
+	r, err := lexer.getRune()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if r != expected {
+		t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
+	}
+
+	for i := 0; i < 100; i++ {
+		if _, err = lexer.getRune(); err != io.EOF {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+	}
+}
+
+func Test_Lexer_peekRune(t *testing.T) {
+	input := "hello world"
+	expected := 'h'
+	lexer := NewLexer(input)
+
+	for i := 0; i < 10; i++ {
+		r, err := lexer.peekRune()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if r != expected {
+			t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
+		}
+	}
+}
+
+func Test_Lexer_peekRune_EOF(t *testing.T) {
+	input := "h"
+	expected := 'h'
+	lexer := NewLexer(input)
+
+	r, err := lexer.getRune()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if r != expected {
+		t.Fatalf("Unexpected rune\nExpected: %q\nActual: %q", expected, r)
+	}
+
+	for i := 0; i < 100; i++ {
+		if _, err = lexer.peekRune(); err != io.EOF {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+	}
+}
+
+func Test_Lexer_getToken(t *testing.T) {
+	testCases := map[string]struct {
+		input          string
+		expectedTokens []Token
+	}{
+		"Whitespace": {
+			input: "  123  ",
+			expectedTokens: []Token{
+				{Type: TokenType_Number, Value: "123"},
+			},
+		},
+		"Number": {
+			input: "123",
+			expectedTokens: []Token{
+				{Type: TokenType_Number, Value: "123"},
+			},
+		},
+		"Label": {
+			input: "fletcher",
+			expectedTokens: []Token{
+				{Type: TokenType_Label, Value: "fletcher"},
+			},
+		},
+		"StringLiteral": {
+			input: `"hello world"`,
+			expectedTokens: []Token{
+				{Type: TokenType_StringLiteral, Value: "hello world"},
+			},
+		},
+		"Plus": {
+			input: "+",
+			expectedTokens: []Token{
+				{Type: TokenType_Plus},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			lexer := NewLexer(tc.input)
+
+			for _, expected := range tc.expectedTokens {
+				tok, err := lexer.GetToken()
+
+				if err != nil {
+					t.Fatalf("Unexpected error: %s", err)
+				}
+
+				if err := _tokensMatch(expected, tok); err != nil {
+					t.Fatalf("Unexpected result: %s", err)
+				}
+			}
+		})
+	}
+}
+
+func Test_Lexer_getToken_EOF(t *testing.T) {
+	input := "  123  "
+	expected := Token{
+		Type:  TokenType_Number,
+		Value: "123",
+	}
+	lexer := NewLexer(input)
+
+	tok, err := lexer.GetToken()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if err := _tokensMatch(expected, tok); err != nil {
+		t.Fatalf("Unexpected result: %s", err)
+	}
+
+	for i := 0; i < 100; i++ {
+		if _, err := lexer.GetToken(); err != io.EOF {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+	}
+}
+
+func Test_Lexer_getToken_InvalidRune(t *testing.T) {
+	input := "  123  `"
+	expected := Token{
+		Type:  TokenType_Number,
+		Value: "123",
+	}
+	lexer := NewLexer(input)
+
+	tok, err := lexer.GetToken()
+
+	if err != nil {
+		t.Fatalf("Unexpected error: %s", err)
+	}
+
+	if err := _tokensMatch(expected, tok); err != nil {
+		t.Fatalf("Unexpected result: %s", err)
+	}
+
+	_, err = lexer.GetToken()
+
+	if err == nil {
+		t.Fatalf("Error expected but not returned")
+	}
+
+	if !errors.Is(err, errInvalidRune) {
+		t.Fatalf("Unexpected result\nExpected: %s\nActual: %s", errInvalidRune, err)
+	}
+}
+
+func Test_Lexer_peekToken(t *testing.T) {
+	input := "123 +"
+	firstExpected := Token{
+		Type:  TokenType_Number,
+		Value: "123",
+	}
+	secondExpected := Token{
+		Type: TokenType_Plus,
+	}
+	lexer := NewLexer(input)
+	shouldBreak := false
+
+	for i := 0; i < 100; i++ {
+		t.Run("First peek", func(t *testing.T) {
+			tok, err := lexer.PeekToken()
+
+			if err != nil {
+				shouldBreak = true
+				t.Fatalf("Unexpected error: %s", err)
+			}
+
+			if err := _tokensMatch(firstExpected, tok); err != nil {
+				shouldBreak = true
+				t.Fatalf("Unexpected result: %s", err)
+			}
+		})
+
+		if shouldBreak {
+			break
+		}
+	}
+
+	t.Run("First get", func(t *testing.T) {
+		tok, err := lexer.GetToken()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if err := _tokensMatch(firstExpected, tok); err != nil {
+			t.Fatalf("Unexpected result: %s", err)
+		}
+	})
+
+	for i := 0; i < 100; i++ {
+		t.Run("Second peek", func(t *testing.T) {
+			tok, err := lexer.PeekToken()
+
+			if err != nil {
+				shouldBreak = true
+				t.Fatalf("Unexpected error: %s", err)
+			}
+
+			if err := _tokensMatch(secondExpected, tok); err != nil {
+				shouldBreak = true
+				t.Fatalf("Unexpected result: %s", err)
+			}
+		})
+
+		if shouldBreak {
+			break
+		}
+	}
+
+	t.Run("Second get", func(t *testing.T) {
+		tok, err := lexer.GetToken()
+
+		if err != nil {
+			t.Fatalf("Unexpected error: %s", err)
+		}
+
+		if err := _tokensMatch(secondExpected, tok); err != nil {
+			t.Fatalf("Unexpected result: %s", err)
+		}
+	})
+}
+
+func Test_Lexer_getTokenStringLiteral_UnexpectedEOF(t *testing.T) {
+	input := `"hello `
+	lexer := NewLexer(input)
+
+	_, err := lexer.GetToken()
+
+	if err == nil {
+		t.Fatalf("Error expected but not returned")
+	}
+
+	if !errors.Is(err, errUnexpectedEOF) {
+		t.Fatalf("Unexpected result\nExpected: %s\nActual: %s", errUnexpectedEOF, err)
+	}
+}
