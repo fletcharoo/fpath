f14a1c6f992b1ec5f28e24a72107fa1e90041129
FletcherCutting <fcutting@hotmail.com>
2025-11-20 23:14:54 +1100

    Tests with testify instead of snapshots

--- internal/runtime/runtime_test.go
+++ internal/runtime/runtime_test.go
@@ -1,37 +1,35 @@
 package runtime_test
 
 import (
-	"os"
 	"testing"
 
 	"github.com/fletcharoo/fpath/internal/lexer"
 	"github.com/fletcharoo/fpath/internal/parser"
 	"github.com/fletcharoo/fpath/internal/runtime"
-	"github.com/gkampitakis/go-snaps/snaps"
+	"github.com/stretchr/testify/require"
 )
-
-func TestMain(m *testing.M) {
-	r := m.Run()
-	snaps.Clean(m, snaps.CleanOpts{Sort: true})
-	os.Exit(r)
-}
 
 func Test_Eval(t *testing.T) {
 	testCases := map[string]struct {
-		query string
-		input any
+		query    string
+		input    any
+		expected float64
 	}{
 		"number": {
-			query: "2",
+			query:    "2",
+			expected: 2.0,
 		},
 		"add": {
-			query: "2 + 3",
+			query:    "2 + 3",
+			expected: 5.0,
 		},
 		"multiply": {
-			query: "2 * 3",
+			query:    "2 * 3",
+			expected: 6.0,
 		},
 		"block": {
-			query: "2 + ((5 * 10) * 2) + 2",
+			query:    "2 + ((5 * 10) * 2) + 2",
+			expected: 104.0,
 		},
 	}
 
@@ -39,21 +37,15 @@ func Test_Eval(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			lex := lexer.New(tc.query)
 			expr, err := parser.New(lex).Parse()
-			if err != nil {
-				t.Fatalf("Unexpected parser error: %s", err)
-			}
+			require.NoError(t, err, "Unexpected parser error")
 
 			result, err := runtime.Eval(expr, err)
-			if err != nil {
-				t.Fatalf("Unexpected runtime error: %s", err)
-			}
+			require.NoError(t, err, "Unexpected runtime error")
 
 			resultDecoded, err := result.Decode()
-			if err != nil {
-				t.Fatalf("Failed to decode result: %s", err)
-			}
+			require.NoError(t, err, "Failed to decode result")
 
-			snaps.MatchSnapshot(t, resultDecoded)
+			require.Equal(t, tc.expected, resultDecoded, "Result does not match expected value")
 		})
 	}
 }
